cd0a213e48683f6e89382a08628ba2ca3f939872
gowricharan <gowricharan16@gmail.com>
2024-07-18 14:28:46 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import Countries from "./Countries";
+import React, {useState} from "react";
 
-function  App() {
+function App() {
+
+  const [count, setCount] = useState(0);
+
+  function inc(){
+    setCount(count+1)
+  }
+
+  function dec(){
+    setCount(count-1)
+  }
+
   return (
     <div className="App">
-      <Countries />
+      <h1>Counter App</h1>
+      <div>Count: {count}</div>
+      <br/>
+      <button onClick={inc}>Increment</button><span> </span><button onClick={dec}>Decrement</button>
     </div>
   );
 }
